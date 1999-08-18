121878bdcc3dbd199c0fcdf586c4e3875dac864f
Monty <xiphmont@mit.edu>
1999-08-18 04:41:00 +0000

    p_block for alpha 7
    git-svn-id: https://svn.xiph.org/trunk/paranoia-III@51 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- paranoia/p_block.c
+++ paranoia/p_block.c
@@ -1,147 +1,187 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "../interface/cdda_interface.h"
 #include "cdda_paranoia.h"
 #include "p_block.h"
 
-void release_p_block(p_block *b){
-  cdrom_paranoia *p=b->p;
-
-  /* yeah, not really needed */
-  b->begin=-1;
-  b->end=-1;
-  b->verifybegin=-1;
-  b->verifyend=-1;
-
-  if(b->buffer){
-    free(b->buffer);
-    b->p->total_bufsize-=b->size;
-    b->buffer=NULL;
-  }
-  b->size=0;
-
-  if(b!=&(p->root)){
-    if(b==p->fragments)
-      p->fragments=b->next;
-    if(b==p->tail)
-      p->tail=b->prev;
-    
-    if(b->prev)
-      b->prev->next=b->next;
-    if(b->next)
-      b->next->prev=b->prev;
-    
-    b->next=p->free;
-    p->free=b;
-  }
-}
-
 /* Get a new block and chain it */
-p_block *new_p_block(cdrom_paranoia *p){
-  p_block *b;
+c_block *new_c_block(cdrom_paranoia *p){
+  c_block *b;
+  c_list  *l=&(p->cache);
+  long size_ptr=sizeof(c_block *);
+  long size=sizeof(c_block);
 
-  /* Are we at/over our allowed cache size? */
-  while(p->total_bufsize>p->cachemark){
-    /* cull from the tail of the list */
-    release_p_block(p->tail);
-  }    
-
-  if(!p->free){
+  if(!l->free){
     /* Oops, need to add a link block. */
     int addto=32;
 
-    if(p->ptr)
-      /* add link blocks */
-      p->ptr=realloc(p->ptr,sizeof(p_block *)*(p->ptrblocks+1));
+    if(l->pool)
+      l->pool=realloc(l->pool,size_ptr*(l->blocks+1));
     else
-      /* no link blocks yet */
-      p->ptr=malloc(sizeof(p_block *));
+      l->pool=malloc(size_ptr);
 
-    p->ptr[p->ptrblocks]=p->free=calloc(addto,sizeof(p_block));
+    l->pool[l->blocks]=l->free=calloc(addto,size);
 
     {
       int i;
       for(i=0;i<addto-1;i++){
-	p->free[i].next=p->free+i+1;
-	p->free[i].p=p;
-	p->free[i].stamp=p->ptrblocks*addto+i+1;
+	l->free[i].next=l->free+i+1;
+	l->free[i].p=p;
+	l->free[i].stamp=l->blocks*addto+i+1;
       }
-      p->free[i].p=p;
-      p->free[i].stamp=p->ptrblocks*addto+i+1;
+      l->free[i].p=p;
+      l->free[i].stamp=l->blocks*addto+i+1;
     }
-    p->ptrblocks++;
+    l->blocks++;
   }
 
-  b=p->free;
-  p->free=b->next;
-
-  if(p->fragments)
-    p->fragments->prev=b;
+  b=l->free;
+  l->free=b->next;
+  if(l->head)
+    l->head->prev=b;
   else
-    p->tail=b;
-    
-  b->next=p->fragments;
+    l->tail=b;    
+  b->next=l->head;
   b->prev=NULL;
-  p->fragments=b;
+  l->head=b;
+  l->current++;
 
-  b->begin=-1;
-  b->end=-1;
-  b->verifybegin=-1;
-  b->verifyend=-1;
-  b->size=0;
-  b->silence=-1;
-  b->offset=0;
   b->lastsector=0;
-  b->done=0;
-
   return(b);
 }
 
-void swap_p_block(p_block *a,p_block *b){
-  p_block t[1];
+v_fragment *new_v_fragment(cdrom_paranoia *p){
+  v_fragment *b;
+  v_list *l=&(p->fragments);
+  long size_ptr=sizeof(v_fragment *);
+  long size=sizeof(v_fragment);
 
-  /* I set things up so a straight memcpy isn't easy.  I'll fix that */
-  t->buffer=a->buffer;
-  t->size=a->size;
-  t->begin=a->begin;
-  t->end=a->end;
-  t->verifybegin=a->verifybegin;
-  t->verifyend=a->verifyend;
-  t->silence=a->silence;
-  t->lastsector=a->lastsector;
-  t->done=a->done;
-  t->offset=a->offset;
+  l->active++;
 
-  a->buffer=b->buffer;
-  a->size=b->size;
-  a->begin=b->begin;
-  a->end=b->end;
-  a->verifybegin=b->verifybegin;
-  a->verifyend=b->verifyend;
-  a->silence=b->silence;
-  a->lastsector=b->lastsector;
-  a->done=b->done;
-  a->offset=b->offset;
+  if(!l->free){
+    /* Oops, need to add a link block. */
+    int addto=32;
 
-  b->buffer=t->buffer;
-  b->size=t->size;
-  b->begin=t->begin;
-  b->end=t->end;
-  b->verifybegin=t->verifybegin;
-  b->verifyend=t->verifyend;
-  b->silence=t->silence;
-  b->lastsector=t->lastsector;
-  b->done=t->done;
-  b->offset=t->offset;
-}
+    if(l->pool)
+      l->pool=realloc(l->pool,size_ptr*(l->blocks+1));
+    else
+      l->pool=malloc(size_ptr);
 
-void p_buffer(p_block *b,size16 *buffer,long size){
-  if(b->buffer){
-    free(b->buffer);
-    b->p->total_bufsize-=b->size;
+    l->pool[l->blocks]=l->free=calloc(addto,size);
+
+    {
+      int i;
+      for(i=0;i<addto-1;i++){
+	l->free[i].next=l->free+i+1;
+	l->free[i].p=p;
+	l->free[i].stamp=l->blocks*addto+i+1;
+      }
+      l->free[i].p=p;
+      l->free[i].stamp=l->blocks*addto+i+1;
+    }
+    l->blocks++;
   }
 
-  b->buffer=buffer;
-  b->size=size;
-  b->p->total_bufsize+=size;
+  b=l->free;
+  l->free=b->next;
+  if(l->head)
+    l->head->prev=b;
+  else
+    l->tail=b;    
+  b->next=l->head;
+  b->prev=NULL;
+  l->head=b;
+
+  b->lastsector=0;
+  return(b);
 }
 
+void release_c_block(c_block *b){
+  cdrom_paranoia *p=b->p;
+  c_list *l=&(p->cache);
+
+#ifdef NOISY
+  fprintf(stderr,"Releasing c_block %d [%ld-%ld]\n",b->stamp,b->begin,b->end);
+#endif
+
+  if(b->buffer){
+    free(b->buffer);
+    free(b->flags);
+    l->current--;
+    b->buffer=NULL;
+    b->flags=NULL;
+  }
+
+  if(b==l->head)
+    l->head=b->next;
+  if(b==l->tail)
+    l->tail=b->prev;
+    
+  if(b->prev)
+    b->prev->next=b->next;
+  if(b->next)
+    b->next->prev=b->prev;
+    
+  b->next=l->free;
+  l->free=b;
+}
+
+void release_v_fragment(v_fragment *b){
+  cdrom_paranoia *p=b->p;
+  v_list *l=&(p->fragments);
+
+  l->active--;
+
+#ifdef NOISY
+  fprintf(stderr,"Releasing v_fragment %d [%ld-%ld]\n",b->stamp,b->begin,
+	  b->end);
+#endif
+
+   if(b==l->head)
+    l->head=b->next;
+  if(b==l->tail)
+    l->tail=b->prev;
+    
+  if(b->prev)
+    b->prev->next=b->next;
+  if(b->next)
+    b->next->prev=b->prev;
+    
+  b->next=l->free;
+  b->one=NULL;
+  b->two=NULL;
+  l->free=b;
+}
+
+void recover_cache(cdrom_paranoia *p){
+  c_list *l=&(p->cache);
+
+  /* Are we at/over our allowed cache size? */
+  while(l->current>l->limit){
+    /* cull from the tail of the list */
+    release_c_block(l->tail);
+  }    
+}
+
+void recover_fragments(cdrom_paranoia *p){
+  v_list *l=&(p->fragments);
+  v_fragment *v=l->head;
+
+  /* Is this verified thingie for a cached block that's been freed? */
+  while(v){
+    v_fragment *next=v->next;
+
+    if(v->one->buffer==NULL)
+      release_v_fragment(v);
+
+    v=next;
+  }    
+}
+
+size16 *v_buffer(v_fragment *v){
+  size16 *buffer=v->one->buffer;
+  long begin=v->one->begin;
+    
+  if(!buffer)return(NULL);
+  return(buffer+v->begin-begin);
+}
--- paranoia/p_block.h
+++ paranoia/p_block.h
@@ -1,9 +1,13 @@
+
 #ifndef _p_block_h_
 #define _p_block_h_
 
-extern void release_p_block(p_block *b);
-extern p_block *new_p_block(cdrom_paranoia *p);
-extern void swap_p_block(p_block *a,p_block *b);
-extern void p_buffer(p_block *b,size16 *buffer,long size);
+extern void release_c_block(c_block *c);
+extern c_block *new_c_block(cdrom_paranoia *p);
+extern void release_v_fragment(v_fragment *c);
+extern v_fragment *new_v_fragment(cdrom_paranoia *p);
+extern size16 *v_buffer(v_fragment *v);
+extern void recover_cache(cdrom_paranoia *p);
+extern void recover_fragments(cdrom_paranoia *p);
 
 #endif
