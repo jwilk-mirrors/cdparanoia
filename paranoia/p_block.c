433f6879e3b8a986dc816deb034c642cad4b2647
Monty <xiphmont@mit.edu>
1999-08-18 04:41:43 +0000

    p_block for alpha 9
    git-svn-id: https://svn.xiph.org/trunk/paranoia-III@53 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- paranoia/p_block.c
+++ paranoia/p_block.c
@@ -1,187 +1,318 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include "p_block.h"
 #include "../interface/cdda_interface.h"
 #include "cdda_paranoia.h"
-#include "p_block.h"
+#include "isort.h"
 
-/* Get a new block and chain it */
-c_block *new_c_block(cdrom_paranoia *p){
-  c_block *b;
-  c_list  *l=&(p->cache);
-  long size_ptr=sizeof(c_block *);
-  long size=sizeof(c_block);
-
-  if(!l->free){
-    /* Oops, need to add a link block. */
-    int addto=32;
-
-    if(l->pool)
-      l->pool=realloc(l->pool,size_ptr*(l->blocks+1));
-    else
-      l->pool=malloc(size_ptr);
-
-    l->pool[l->blocks]=l->free=calloc(addto,size);
-
-    {
-      int i;
-      for(i=0;i<addto-1;i++){
-	l->free[i].next=l->free+i+1;
-	l->free[i].p=p;
-	l->free[i].stamp=l->blocks*addto+i+1;
-      }
-      l->free[i].p=p;
-      l->free[i].stamp=l->blocks*addto+i+1;
-    }
-    l->blocks++;
-  }
-
-  b=l->free;
-  l->free=b->next;
-  if(l->head)
-    l->head->prev=b;
-  else
-    l->tail=b;    
-  b->next=l->head;
-  b->prev=NULL;
-  l->head=b;
-  l->current++;
-
-  b->lastsector=0;
-  return(b);
+linked_list *new_list(void *(*newp)(void),void (*freep)(void *)){
+  linked_list *ret=calloc(1,sizeof(linked_list));
+  ret->new_poly=newp;
+  ret->free_poly=freep;
+  return(ret);
 }
 
-v_fragment *new_v_fragment(cdrom_paranoia *p){
-  v_fragment *b;
-  v_list *l=&(p->fragments);
-  long size_ptr=sizeof(v_fragment *);
-  long size=sizeof(v_fragment);
+linked_element *add_elem(linked_list *l,void *elem){
 
+  linked_element *ret=calloc(1,sizeof(linked_element));
+  ret->stamp=l->current++;
+  ret->ptr=elem;
+  ret->list=l;
+
+  if(l->head)
+    l->head->prev=ret;
+  else
+    l->tail=ret;    
+  ret->next=l->head;
+  ret->prev=NULL;
+  l->head=ret;
   l->active++;
 
-  if(!l->free){
-    /* Oops, need to add a link block. */
-    int addto=32;
+  return(ret);
+}
 
-    if(l->pool)
-      l->pool=realloc(l->pool,size_ptr*(l->blocks+1));
-    else
-      l->pool=malloc(size_ptr);
+linked_element *new_elem(linked_list *list){
+  void *new=list->new_poly();
+  return(add_elem(list,new));
+}
 
-    l->pool[l->blocks]=l->free=calloc(addto,size);
+void free_elem(linked_element *e,int free_ptr){
+  linked_list *l=e->list;
+  if(free_ptr)l->free_poly(e->ptr);
 
-    {
-      int i;
-      for(i=0;i<addto-1;i++){
-	l->free[i].next=l->free+i+1;
-	l->free[i].p=p;
-	l->free[i].stamp=l->blocks*addto+i+1;
-      }
-      l->free[i].p=p;
-      l->free[i].stamp=l->blocks*addto+i+1;
-    }
-    l->blocks++;
+  if(e==l->head)
+    l->head=e->next;
+  if(e==l->tail)
+    l->tail=e->prev;
+    
+  if(e->prev)
+    e->prev->next=e->next;
+  if(e->next)
+    e->next->prev=e->prev;
+
+  l->active--;
+  free(e);
+} 
+
+void free_list(linked_list *list,int free_ptr){
+  while(list->head)
+    free_elem(list->head,free_ptr);
+  free(list);
+}
+
+void *get_elem(linked_element *e){
+  return(e->ptr);
+}
+
+linked_list *copy_list(linked_list *list){
+  linked_list *new=new_list(list->new_poly,list->free_poly);
+  linked_element *i=list->tail;
+
+  while(i){
+    add_elem(new,i->ptr);
+    i=i->prev;
   }
+  return(new);
+}
 
-  b=l->free;
-  l->free=b->next;
-  if(l->head)
-    l->head->prev=b;
-  else
-    l->tail=b;    
-  b->next=l->head;
-  b->prev=NULL;
-  l->head=b;
+/**** C_block stuff ******************************************************/
 
-  b->lastsector=0;
+static c_block *i_cblock_constructor(cdrom_paranoia *p){
+  c_block *ret=calloc(1,sizeof(c_block));
+  return(ret);
+}
+
+static void i_cblock_destructor(c_block *c){
+  if(c->vector)isort_free(c->vector);
+  if(c->flags)free(c->flags);
+  c->e=NULL;
+  free(c);
+}
+
+c_block *new_c_block(cdrom_paranoia *p){
+  linked_element *e=new_elem(p->cache);
+  c_block *c=e->ptr;
+  c->e=e;
+  c->p=p;
+  return(c);
+}
+
+void free_c_block(c_block *c){
+  /* also rid ourselves of v_fragments that reference this block */
+  v_fragment *v=v_first(c->p);
+  
+  while(v){
+    v_fragment *next=v_next(v);
+    if(v->one==c)free_v_fragment(v);
+    v=next;
+  }    
+
+  free_elem(c->e,1);
+}
+
+static v_fragment *i_vfragment_constructor(void){
+  v_fragment *ret=calloc(1,sizeof(v_fragment));
+  return(ret);
+}
+
+static void i_v_fragment_destructor(v_fragment *v){
+  free(v);
+}
+
+v_fragment *new_v_fragment(cdrom_paranoia *p,c_block *one,
+			   long begin, long end, int last){
+  linked_element *e=new_elem(p->fragments);
+  v_fragment *b=e->ptr;
+  
+  b->e=e;
+  b->p=p;
+
+  b->one=one;
+  b->begin=begin;
+  b->size=end-begin;
+  b->lastsector=last;
+
   return(b);
 }
 
-void release_c_block(c_block *b){
-  cdrom_paranoia *p=b->p;
-  c_list *l=&(p->cache);
-
-#ifdef NOISY
-  fprintf(stderr,"Releasing c_block %d [%ld-%ld]\n",b->stamp,b->begin,b->end);
-#endif
-
-  if(b->buffer){
-    free(b->buffer);
-    free(b->flags);
-    l->current--;
-    b->buffer=NULL;
-    b->flags=NULL;
-  }
-
-  if(b==l->head)
-    l->head=b->next;
-  if(b==l->tail)
-    l->tail=b->prev;
-    
-  if(b->prev)
-    b->prev->next=b->next;
-  if(b->next)
-    b->next->prev=b->prev;
-    
-  b->next=l->free;
-  l->free=b;
+void free_v_fragment(v_fragment *v){
+  free_elem(v->e,1);
 }
 
-void release_v_fragment(v_fragment *b){
-  cdrom_paranoia *p=b->p;
-  v_list *l=&(p->fragments);
+c_block *c_first(cdrom_paranoia *p){
+  if(p->cache->head)
+    return(p->cache->head->ptr);
+  return(NULL);
+}
 
-  l->active--;
+c_block *c_last(cdrom_paranoia *p){
+  if(p->cache->tail)
+    return(p->cache->tail->ptr);
+  return(NULL);
+}
 
-#ifdef NOISY
-  fprintf(stderr,"Releasing v_fragment %d [%ld-%ld] %ld still active\n",
-	  b->stamp,b->begin,b->end,l->active);
-#endif
+c_block *c_next(c_block *c){
+  if(c->e->next)
+    return(c->e->next->ptr);
+  return(NULL);
+}
 
-   if(b==l->head)
-    l->head=b->next;
-  if(b==l->tail)
-    l->tail=b->prev;
-    
-  if(b->prev)
-    b->prev->next=b->next;
-  if(b->next)
-    b->next->prev=b->prev;
-    
-  b->next=l->free;
-  b->one=NULL;
-  b->two=NULL;
-  l->free=b;
+c_block *c_prev(c_block *c){
+  if(c->e->prev)
+    return(c->e->prev->ptr);
+  return(NULL);
+}
+
+v_fragment *v_first(cdrom_paranoia *p){
+  if(p->fragments->head)
+    return(p->fragments->head->ptr);
+  return(NULL);
+}
+
+v_fragment *v_last(cdrom_paranoia *p){
+  if(p->fragments->tail)
+    return(p->fragments->tail->ptr);
+  return(NULL);
+}
+
+v_fragment *v_next(v_fragment *v){
+  if(v->e->next)
+    return(v->e->next->ptr);
+  return(NULL);
+}
+
+v_fragment *v_prev(v_fragment *v){
+  if(v->e->prev)
+    return(v->e->prev->ptr);
+  return(NULL);
 }
 
 void recover_cache(cdrom_paranoia *p){
-  c_list *l=&(p->cache);
+  linked_list *l=p->cache;
 
   /* Are we at/over our allowed cache size? */
-  while(l->current>l->limit){
+  while(l->active>p->cache_limit)
     /* cull from the tail of the list */
-    release_c_block(l->tail);
-  }    
-}
+    free_c_block(c_last(p));
 
-void recover_fragments(cdrom_paranoia *p){
-  v_list *l=&(p->fragments);
-  v_fragment *v=l->head;
-
-  /* Is this verified thingie for a cached block that's been freed? */
-  while(v){
-    v_fragment *next=v->next;
-
-    if(v->one==NULL || v->two==NULL || v->one->buffer==NULL)
-      release_v_fragment(v);
-
-    v=next;
-  }    
 }
 
 size16 *v_buffer(v_fragment *v){
-  size16 *buffer=v->one->buffer;
-  long begin=v->one->begin;
+  size16 *buffer=isort_buffer(v->one->vector);
+  long begin=isort_begin(v->one->vector);
     
   if(!buffer)return(NULL);
   return(buffer+v->begin-begin);
 }
+
+/**** Initialization *************************************************/
+
+void i_paranoia_firstlast(cdrom_paranoia *p){
+  int i;
+  cdrom_drive *d=p->d;
+  p->current_lastsector=-1;
+  for(i=cdda_sector_gettrack(d,p->cursor);i<cdda_tracks(d);i++)
+    if(!cdda_track_audiop(d,i))
+      p->current_lastsector=cdda_track_lastsector(d,i-1);
+  if(p->current_lastsector==-1)
+    p->current_lastsector=cdda_disc_lastsector(d);
+
+  p->current_firstsector=-1;
+  for(i=cdda_sector_gettrack(d,p->cursor);i>0;i--)
+    if(!cdda_track_audiop(d,i))
+      p->current_firstsector=cdda_track_firstsector(d,i+1);
+  if(p->current_firstsector==-1)
+    p->current_firstsector=cdda_disc_firstsector(d);
+
+}
+
+cdrom_paranoia *paranoia_init(cdrom_drive *d){
+  cdrom_paranoia *p=calloc(1,sizeof(cdrom_paranoia));
+
+  p->cache=new_list((void *)&i_cblock_constructor,
+		    (void *)&i_cblock_destructor);
+
+  p->fragments=new_list((void *)&i_vfragment_constructor,
+			(void *)&i_v_fragment_destructor);
+
+  p->d=d;
+  p->readahead=150;
+  p->dynoverlap=512;
+  p->cache_limit=JIGGLE_MODULO;
+  p->enable=PARANOIA_MODE_FULL;
+  p->cursor=cdda_disc_firstsector(d);
+  p->lastread=LONG_MAX;
+
+  /* One last one... in case data and audio tracks are mixed... */
+  i_paranoia_firstlast(p);
+
+  return(p);
+}
+
+#ifdef TEST
+
+#undef TEST
+#include "isort.c"
+
+typedef struct dummyel{
+  int foo;
+} dummyel;
+
+dummyel *new_dummy(){
+  return(calloc(1,sizeof(dummyel)));
+}
+
+int main(){
+  linked_list *tl;
+  linked_element *one,*two,*three,*four;
+
+  tl=new_list((void *)&new_dummy,&free);
+  one=new_elem(tl);
+  two=new_elem(tl);
+  three=new_elem(tl);
+  four=new_elem(tl);
+
+  if(tl->head!=four){
+    printf("linked lists: failed test 1\n");
+    return(1);
+  }
+  if(tl->tail!=one){
+    printf("linked lists: failed test 2\n");
+    return(1);
+  }
+  if(tl->active!=4){
+    printf("linked lists: failed test 3\n");
+    return(1);
+  }
+
+  free_elem(one,1);
+  free_elem(four,1);
+
+  if(tl->head!=three){
+    printf("linked lists: failed test 4\n");
+    return(1);
+  }
+  if(tl->tail!=two){
+    printf("linked lists: failed test 5\n");
+    return(1);
+  }
+
+
+  if(tl->active!=2){
+    printf("linked lists: failed test 6\n");
+    return(1);
+  }
+  one=new_elem(tl);
+  if(tl->active!=3){
+    printf("linked lists: failed test 7\n");
+    return(1);
+  }
+
+  free_list(tl,1);
+
+  return(0);
+}
+
+#endif
--- paranoia/p_block.h
+++ paranoia/p_block.h
@@ -1,13 +1,159 @@
+/***
+ * CopyPolicy: GNU Public License 2 applies
+ * Copyright (C) by Monty (xiphmont@mit.edu)
+ ***/
 
 #ifndef _p_block_h_
 #define _p_block_h_
 
-extern void release_c_block(c_block *c);
-extern c_block *new_c_block(cdrom_paranoia *p);
-extern void release_v_fragment(v_fragment *c);
-extern v_fragment *new_v_fragment(cdrom_paranoia *p);
-extern size16 *v_buffer(v_fragment *v);
-extern void recover_cache(cdrom_paranoia *p);
-extern void recover_fragments(cdrom_paranoia *p);
+#include "../interface/cdda_interface.h"
 
+#define MIN_WORDS_OVERLAP    64     /* 16 bit words */
+#define MIN_WORDS_SEARCH     64     /* 16 bit words */
+#define MIN_WORDS_RIFT       16     /* 16 bit words */
+#define MAX_SECTOR_OVERLAP   32     /* sectors */
+#define MIN_SECTOR_EPSILON  128     /* words */
+#define MIN_SECTOR_BACKUP    16     /* sectors */
+#define JIGGLE_MODULO         8     /* sectors */
+
+#define min(x,y) ((x)>(y)?(y):(x))
+#define max(x,y) ((x)<(y)?(y):(x))
+
+typedef struct linked_list{
+  /* linked list */
+  struct linked_element *head;
+  struct linked_element *tail;
+
+  void *(*new_poly)();
+  void (*free_poly)(void *poly);
+  long current;
+  long active;
+
+} linked_list;
+
+typedef struct linked_element{
+  void *ptr;
+  struct linked_element *prev;
+  struct linked_element *next;
+  
+  struct linked_list *list;
+  int stamp;
+} linked_element;
+
+extern linked_list *new_list(void *(*new)(void),void (*free)(void *));
+extern linked_element *new_elem(linked_list *list);
+extern linked_element *add_elem(linked_list *list,void *elem);
+extern void free_list(linked_list *list,int free_ptr); /* unlink or free */
+extern void free_elem(linked_element *e,int free_ptr); /* unlink or free */
+extern void *get_elem(linked_element *e);
+extern linked_list *copy_list(linked_list *list); /* shallow; doesn't copy
+						     contained structures */
+
+typedef struct c_block{
+  /* The buffer */
+  void *vector;  /* (is_vector *) */
+
+  /* auxiliary support structures */
+  unsigned char *flags; /* 1    known boundaries in read data
+			   2    known blanked data
+			   4    used sample
+			   8    reserved
+			   16   reserved
+			   32   reserved
+			   64   reserved
+			   128  reserved
+			 */
+
+  /* end of session cases */
+  long lastsector;
+  struct cdrom_paranoia *p;
+  struct linked_element *e;
+
+} c_block;
+
+extern void free_c_block(c_block *c);
+extern c_block *new_c_block(struct cdrom_paranoia *p);
+
+typedef struct v_fragment{
+  c_block *one;
+  
+  long begin;
+  long size;
+
+  /* end of session cases */
+  long lastsector;
+
+  /* linked list */
+  struct cdrom_paranoia *p;
+  struct linked_element *e;
+
+} v_fragment;
+
+extern void free_v_fragment(v_fragment *c);
+extern v_fragment *new_v_fragment(struct cdrom_paranoia *p,c_block *one,
+				  long begin, long end, int lastsector);
+extern size16 *v_buffer(v_fragment *v);
+
+extern c_block *c_first(struct cdrom_paranoia *p);
+extern c_block *c_last(struct cdrom_paranoia *p);
+extern c_block *c_next(c_block *c);
+extern c_block *c_prev(c_block *c);
+
+extern v_fragment *v_first(struct cdrom_paranoia *p);
+extern v_fragment *v_last(struct cdrom_paranoia *p);
+extern v_fragment *v_next(v_fragment *v);
+extern v_fragment *v_prev(v_fragment *v);
+
+typedef struct root_block{
+  long returnedlimit;   
+  long lastsector;
+  struct cdrom_paranoia *p;
+
+  void *vector;
+} root_block;
+
+typedef struct offsets{
+  
+  long offpoints;
+  long newpoints;
+  long offaccum;
+  long offdiff;
+  long offmin;
+  long offmax;
+
+} offsets;
+
+typedef struct cdrom_paranoia{
+  cdrom_drive *d;
+
+  root_block root;        /* verified/reconstructed cached data */
+  linked_list *cache;     /* our data as read from the cdrom */
+  long cache_limit;
+  linked_list *fragments; /* fragments of blocks that have been 'verified' */
+
+  int readahead;          /* sectors of readahead in each readop */
+  int jitter;           
+  long lastread;
+
+  int enable;
+  long cursor;
+  long current_lastsector;
+  long current_firstsector;
+
+  /* statistics for drift/overlap */
+  struct offsets stage1;
+  struct offsets stage2;
+
+  long dynoverlap;
+  long dyndrift;
+
+  /* statistics for verification */
+
+} cdrom_paranoia;
+
+extern void recover_cache(cdrom_paranoia *p);
+extern void i_paranoia_firstlast(cdrom_paranoia *p);
+
+#define CDP_COMPILE
 #endif
+
