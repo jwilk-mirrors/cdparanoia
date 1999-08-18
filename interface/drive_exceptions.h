8f4c3bc72b5b6e0806d0455f822b56056c94a757
Monty <xiphmont@mit.edu>
1999-08-18 04:32:06 +0000

    Updated for alpha 9.3
    git-svn-id: https://svn.xiph.org/trunk/paranoia-III@48 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- interface/drive_exceptions.h
+++ interface/drive_exceptions.h
@@ -14,7 +14,6 @@ typedef struct exception {
   int  (*enable)(struct cdrom_drive *,int);
   long (*read)(struct cdrom_drive *,void *,long,long);
   int  bigendianp;
-  int  ignore_toc_offset;
 } exception;
 
 /* specific to general */
@@ -23,40 +22,39 @@ typedef struct exception {
    (force drives to detect as ATAPI or SCSI, force ATAPI read command */
 
 static exception atapi_list[]={
-  {"TOSHIBA CD-ROM XM-5702B",       -1,0x82,         Dummy,          NULL,0,1},
-  {"SAMSUNG SCR-830 REV 2.09 2.09 ", 1,   0,         Dummy,scsi_read_mmc2,0,0},
-  {"Memorex CR-622",                 1,   0,         Dummy,          NULL,0,0},
+  {"SAMSUNG SCR-830 REV 2.09 2.09 ", 1,   0,         Dummy,scsi_read_mmc2,0},
+  {"Memorex CR-622",                 1,   0,         Dummy,          NULL,0},
   {NULL,0,0,NULL,NULL,0}};
 
 /* list of drives that affect MMC default settings */
 
 static exception mmc_list[]={
-  {"SAMSUNG SCR-830 REV 2.09 2.09 ", 1,   0,         Dummy,scsi_read_mmc2,0,0},
-  {"Memorex CR-622",                 1,   0,         Dummy,          NULL,0,0},
+  {"SAMSUNG SCR-830 REV 2.09 2.09 ", 1,   0,         Dummy,scsi_read_mmc2,0},
+  {"Memorex CR-622",                 1,   0,         Dummy,          NULL,0},
   {NULL,0,0,NULL,NULL,0}};
 
 /* list of drives that affect SCSI default settings */
 
 static exception scsi_list[]={
-  {"TOSHIBA",                     -1,0x82,scsi_enable_cdda,scsi_read_28,  0,0},
-  {"IBM",                         -1,0x82,scsi_enable_cdda,scsi_read_28,  0,0},
-  {"DEC",                         -1,0x82,scsi_enable_cdda,scsi_read_28,  0,0},
+  {"TOSHIBA",                     -1,0x82,scsi_enable_cdda,scsi_read_28,  0},
+  {"IBM",                         -1,0x82,scsi_enable_cdda,scsi_read_28,  0},
+  {"DEC",                         -1,0x82,scsi_enable_cdda,scsi_read_28,  0},
   
-  {"IMS",                         -1,   0,scsi_enable_cdda,scsi_read_28,  1,0},
-  {"KODAK",                       -1,   0,scsi_enable_cdda,scsi_read_28,  1,0},
-  {"RICOH",                       -1,   0,scsi_enable_cdda,scsi_read_28,  1,0},
-  {"HP",                          -1,   0,scsi_enable_cdda,scsi_read_28,  1,0},
-  {"PHILIPS",                     -1,   0,scsi_enable_cdda,scsi_read_28,  1,0},
-  {"PLASMON",                     -1,   0,scsi_enable_cdda,scsi_read_28,  1,0},
-  {"GRUNDIG CDR100IPW",           -1,   0,scsi_enable_cdda,scsi_read_28,  1,0},
-  {"MITSUMI CD-R ",               -1,   0,scsi_enable_cdda,scsi_read_28,  1,0},
+  {"IMS",                         -1,   0,scsi_enable_cdda,scsi_read_28,  1},
+  {"KODAK",                       -1,   0,scsi_enable_cdda,scsi_read_28,  1},
+  {"RICOH",                       -1,   0,scsi_enable_cdda,scsi_read_28,  1},
+  {"HP",                          -1,   0,scsi_enable_cdda,scsi_read_28,  1},
+  {"PHILIPS",                     -1,   0,scsi_enable_cdda,scsi_read_28,  1},
+  {"PLASMON",                     -1,   0,scsi_enable_cdda,scsi_read_28,  1},
+  {"GRUNDIG CDR100IPW",           -1,   0,scsi_enable_cdda,scsi_read_28,  1},
+  {"MITSUMI CD-R ",               -1,   0,scsi_enable_cdda,scsi_read_28,  1},
 
-  {"YAMAHA",                      -1,   0,scsi_enable_cdda,        NULL,  0,0},
+  {"YAMAHA",                      -1,   0,scsi_enable_cdda,        NULL,  0},
 
-  {"PLEXTOR",                     -1,   0,            NULL,        NULL,  0,0},
-  {"SONY",                        -1,   0,            NULL,        NULL,  0,0},
+  {"PLEXTOR",                     -1,   0,            NULL,        NULL,  0},
+  {"SONY",                        -1,   0,            NULL,        NULL,  0},
 
-  {"NEC",                         -1,   0,           NULL,scsi_read_D4_10,0,0},
+  {"NEC",                         -1,   0,           NULL,scsi_read_D4_10,0},
 
   {NULL,0,0,NULL,NULL,0}};
 
